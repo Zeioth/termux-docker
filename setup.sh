72c88264703f97110283b0febbe35b95930d6892
Zeioth <zeioth@hotmail.com>
2023-04-02 14:53:14 +0200

    Setup refactor

--- setup.sh
+++ setup.sh
@@ -2,23 +2,24 @@
 ## This file install the necessary dependencies
 
 
-# Install dependencies
-#pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless openssh
+# Install dependencies - (comment if you are not on termux)
+pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless openssh
 
 # Change this constant to install somewhere else if you want.
 INSTALL_DIR="$HOME/alpine"
 mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
 
 # Download necessary files
-wget -q -c -t0 "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/config.env"
-wget -q -c -t0 "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/ssh2qemu.sh"
-wget -q -c -t0 "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/startqemu.sh"
-wget -q -c -t0 "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/installqemu.expect"
+curl "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/config.env" > "$INSTALL_DIR/config.env"
+curl "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/ssh2qemu.sh" > "$INSTALL_DIR/ssh2qemu.sh"
+curl "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/startqemu.sh" > "$INSTALL_DIR/startqemu.sh"
+curl "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/installqemu.expect" > "$INSTALL_DIR/installqemu.expect"
+curl "https://raw.githubusercontent.com/Zeioth/termux-docker/main/alpine/answerfile" > "$INSTALL_DIR/answerfile"
 
 # Give execution permissions
-chmod +x ./ssh2qemu.sh
-chmod +x ./startqemu.sh
+chmod +x "$INSTALL_DIR/ssh2qemu.sh"
+chmod +x "$INSTALL_DIR/startqemu.sh"
 
-# run installqemu
-. config.env
-expect -f installqemu.expect
+# Run installqemu
+. "$INSTALL_DIR/config.env"
+expect -f "$INSTALL_DIR/installqemu.expect"
